061a637dc232a6c6ed2116bbd7d18b7750635f48
Rodrigo Xavier <rodrigoxc97@hotmail.com>
2025-07-21 15:34:40 -0300

    adjusts no controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,4 @@
 import { Controller } from '@nestjs/common';
-import { UsersRepository } from './repositories/user/users-repository';
 
 @Controller()
-export class AppController {
-  constructor(private usersRepository: UsersRepository) {}
-}
+export class AppController {}
